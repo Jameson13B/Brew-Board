f2762658a3d5e961e45a8ce4f639a3a01e6c1898
Jameson.Brown <jameson.brown@mx.com>
2020-11-14 21:37:10 -0700

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { Switch, Route } from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div style={{ textAlign: 'center' }}>
+        <h2>Welcome to New FE App</h2>
+        <Switch>
+          <Route exact path="/" component={() => <p>See README for details</p>} />
+          <Route component={() => <p>404: Navigate to Root</p>} />
+        </Switch>
+      </div>
+    )
+  }
 }
-
-export default App;
+export default App
