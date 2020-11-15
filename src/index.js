6d7e42cbaf0c1d7cf21a9276e2d326ee15db7288
Jameson.Brown <jameson.brown@mx.com>
2020-11-15 00:11:35 -0700

    Removed router wrapper

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter as Router } from 'react-router-dom'
 import { ElefantProvider } from 'elefant-state'
 
 import './index.css'
@@ -10,10 +9,8 @@ import { initialState, reducer } from './State'
 require('dotenv').config()
 
 ReactDOM.render(
-  <Router>
-    <ElefantProvider reducer={reducer} initialState={initialState}>
-      <App />
-    </ElefantProvider>
-  </Router>,
+  <ElefantProvider reducer={reducer} initialState={initialState}>
+    <App />
+  </ElefantProvider>,
   document.getElementById('root'),
 )
